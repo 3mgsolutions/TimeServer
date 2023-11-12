a802fdacb395e91d6007aadbbde5f8c74c61aec9
mmitev <martin.mitev@amdaris.com>
2023-11-12 09:24:35 +0200

    deploy as a windows service

--- TimeServer.Service/Program.cs
+++ TimeServer.Service/Program.cs
@@ -1,17 +1,32 @@
 using TimeServer.Service.Services;
+using Microsoft.Extensions.Hosting.WindowsServices;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
-// Add services to the container.
+// Use WebApplicationOptions to set the ContentRootPath
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService()
+        ? AppContext.BaseDirectory
+        : default
+});
+
+// Set WindowsServiceLifetime
+builder.Host.UseWindowsService();
+
 builder.Services.AddGrpc();
+
+// Add reflection services
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+
+// Map reflection endpoint
+app.MapGrpcReflectionService();
 
 app.Run();
